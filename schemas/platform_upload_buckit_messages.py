d74b132703ae9888a6cfb9f5c41c08c797115e04
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 13:27:12 +0100

    Processing part

--- schemas/platform_upload_buckit_messages.py
+++ schemas/platform_upload_buckit_messages.py
@@ -16,6 +16,7 @@
 
 """Validator for messages stored in platform.upload.buckit topic."""
 
+import json
 import sys
 import os
 from argparse import ArgumentParser
@@ -30,13 +31,74 @@ def cli_arguments():
                         action="store", default=None, type=str, required=True)
     parser.add_argument("-m", "--multiple", dest="multiple",
                         help="Input file should containg more messages, each message on one line",
-                        action="store", default=False, type=bool, required=False)
+                        action="store_true", default=False, required=False)
     parser.add_argument("-v", "--verbose", dest="verbose", help="make it verbose",
-                        action="store_true", default=None)
+                        action="store_true", default=None, required=False)
 
     # Now it is time to parse flags, check the actual content of command line
     # and fill in the object named `args`.
     return parser.parse_args()
+
+
+def load_json_from_file(filename, verbose):
+    """Load and decode JSON file."""
+    if verbose:
+        print("Loading original file", filename)
+    with open(filename) as fin:
+        return json.load(fin)
+
+
+def validate(payload, verbose):
+    """Try to validate the payload against known schema."""
+    pass
+
+
+def validate_single_message(input_file, verbose):
+    """Validate single message stored in input file."""
+    try:
+        payload = load_json_from_file(input_file, verbose)
+        valid = validate(payload, verbose)
+        return {"processed": 1,
+                "valid": 1 if valid else 0,
+                "error": 0}
+    except Exception as e:
+        print(e)
+        return {"processed": 1,
+                "valid": 0,
+                "error": 1}
+
+
+def try_to_validate_message(line, processed, verbose):
+    """Try to validate one message represented by string."""
+    if verbose:
+        print("Reading message #{}".format(processed))
+    try:
+        payload = json.loads(line)
+        return validate(payload, verbose), False
+    except Exception as e:
+        print(e)
+        return False, True
+
+
+def validate_multiple_messages(input_file, verbose):
+    """Validate multiple messages stored in input file."""
+    processed = 0
+    valid = 0
+    error = 0
+
+    with open(input_file, "r") as fin:
+        # iterate over all lines in the message file
+        for line in fin:
+            processed += 1
+            v, e = try_to_validate_message(line, processed, verbose)
+            if v:
+                valid += 1
+            if e:
+                error += 1
+
+    return {"processed": processed,
+            "valid": valid,
+            "error": error}
 
 
 def main():
@@ -45,6 +107,13 @@ def main():
     args = cli_arguments()
     verbose = args.verbose
     multiple = args.multiple
+    input_file = args.input
+
+    if multiple:
+        report = validate_multiple_messages(input_file, verbose)
+    else:
+        report = validate_single_message(input_file, verbose)
+    print(report)
 
 
 if __name__ == "__main__":
