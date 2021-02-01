ed782caa336f8839343dd480ff27663a32bf1782
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-01 10:00:38 +0100

    Refactor platform_upload_buckit_messages.py to use common module

--- schemas/platform_upload_buckit_messages.py
+++ schemas/platform_upload_buckit_messages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8
 
-# Copyright © 2020 Pavel Tisnovsky
+# Copyright © 2020, 2021 Pavel Tisnovsky
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,17 +17,17 @@
 
 """Validator for messages stored in platform.upload.buckit topic."""
 
-import json
 import sys
-from os import popen
-from argparse import ArgumentParser
 
 from voluptuous import Schema
-from voluptuous import Invalid
 from voluptuous import Required
 from voluptuous import ALLOW_EXTRA
 
 from validators import *
+
+from common import read_control_code, cli_arguments, load_json_from_file
+from common import validate_single_message, validate_multiple_messages
+from common import print_report
 
 
 identitySchema = Schema(
@@ -62,137 +62,6 @@ schema = Schema(
          Required("b64_identity"): lambda value: b64IdentityValidator(identitySchema, value),
          Required("timestamp"): timestampValidatorMs,
          })
-
-
-def read_control_code(operation):
-    """Try to execute tput to read control code for selected operation."""
-    return popen("tput " + operation, "r").readline()
-
-
-def cli_arguments():
-    """Retrieve all CLI arguments."""
-    # First of all, we need to specify all command line flags that are
-    # recognized by this tool.
-    parser = ArgumentParser()
-    parser.add_argument("-i", "--input", dest="input", help="name of input file",
-                        action="store", default=None, type=str, required=True)
-    parser.add_argument("-m", "--multiple", dest="multiple",
-                        help="Input file should containg more messages, each message on one line",
-                        action="store_true", default=False, required=False)
-    parser.add_argument("-n", "--no-colors", dest="nocolors", help="disable color output",
-                        action="store_true", default=None)
-    parser.add_argument("-v", "--verbose", dest="verbose", help="make it verbose",
-                        action="store_true", default=None, required=False)
-
-    # Now it is time to parse flags, check the actual content of command line
-    # and fill in the object named `args`.
-    return parser.parse_args()
-
-
-def load_json_from_file(filename, verbose):
-    """Load and decode JSON file."""
-    if verbose:
-        print("Loading original file", filename)
-    with open(filename) as fin:
-        return json.load(fin)
-
-
-def validate(schema, payload, verbose):
-    """Try to validate the payload against known schema."""
-    if payload is None:
-        raise ValueError("Empty payload")
-    schema(payload)
-
-
-def validate_single_message(schema, input_file, verbose):
-    """Validate single message stored in input file."""
-    processed = 0
-    valid = 0
-    error = 0
-
-    try:
-        payload = load_json_from_file(input_file, verbose)
-        processed = 1
-        validate(schema, payload, verbose)
-        valid = 1
-    except (ValueError, Invalid) as ve:
-        print("Validation error: " + str(ve))
-    except Exception as e:
-        print("Other problem: " + str(e))
-        error = 1
-
-    return {"processed": processed,
-            "valid": valid,
-            "error": error}
-
-
-def try_to_validate_message(schema, line, processed, verbose):
-    """Try to validate one message represented by string."""
-    if verbose:
-        print("Reading message #{}".format(processed))
-    # load the payload from string
-    payload = json.loads(line)
-    # and try to validate it
-    validate(schema, payload, verbose)
-
-
-def validate_multiple_messages(schema, input_file, verbose):
-    """Validate multiple messages stored in input file."""
-    processed = 0
-    valid = 0
-    error = 0
-
-    with open(input_file, "r") as fin:
-        # iterate over all lines in the message file
-        for line in fin:
-            processed += 1
-            try:
-                try_to_validate_message(schema, line, processed, verbose)
-                valid += 1
-            except (ValueError, Invalid) as ve:
-                print("Validation error: " + str(ve))
-            except Exception as e:
-                print("Other problem: " + str(e))
-                error += 1
-
-    return {"processed": processed,
-            "valid": valid,
-            "error": error}
-
-
-def print_report(report, nocolors):
-    """Display report about number of passes and failures."""
-    # First of all, we need to setup colors to be displayed on terminal. Colors
-    # are displayed by using terminal escape control codes. When color output
-    # are not enabled on command line, we can simply use empty strings in
-    # output instead of real color escape codes.
-    red_background = green_background = magenta_background = red_foreground = \
-        green_foreground = blue_foreground = no_color = ""
-
-    # If colors are enabled by command line parameter, use control sequence
-    # returned by `tput` command.
-    if not nocolors:
-        red_background = read_control_code("setab 1")
-        green_background = read_control_code("setab 2")
-        magenta_background = read_control_code("setab 5")
-        red_foreground = read_control_code("setaf 1")
-        blue_foreground = read_control_code("setaf 4")
-        green_foreground = read_control_code("setaf 2")
-        no_color = read_control_code("sgr0")
-
-    print("\nStatus:")
-    print("Processed messages: {}{}{}".format(green_foreground, report["processed"], no_color))
-    print("Valid messages:     {}{}{}".format(blue_foreground, report["valid"], no_color))
-    print("Errors detected:    {}{}{}".format(red_foreground, report["error"], no_color))
-    print("\nSummary:")
-
-    if report["error"] == 0:
-        if report["processed"] == report["valid"]:
-            print("{}[OK]{}: all messages have proper format".format(green_background, no_color))
-        else:
-            print("{}[WARN]{}: invalid messages detected".format(magenta_background, no_color))
-    else:
-        print("{}[FAIL]{}: invalid JSON(s) detected".format(red_background, no_color))
 
 
 def main():
