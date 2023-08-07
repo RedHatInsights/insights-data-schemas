7734bc4dc67aca4c4f3424a062da802889df8562
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-07 09:37:57 +0200

    Make linter happy

--- schemas/parquet_output_conditional_update_risks.py
+++ schemas/parquet_output_conditional_update_risks.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Validator for messages produced by Parquet factory into conditional_update_risks.parquet files."""
+"""Validator for messages produced by Parquet factory into conditional_update_risks.parquet files."""  # noqa E501
 
 
 from voluptuous import Schema
